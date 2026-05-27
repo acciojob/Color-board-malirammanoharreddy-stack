e7bf92db6676b1caf0764d5a240debb718d295f5
RAM MANOHAR REDDY <malirammanoharreddy@gmail.com>
2026-05-27 12:48:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+ const container = document.querySelector(".container"); 
+        const total = 800 ;
+        for(let i = 0 ; i<800;i++){
+            const box = document.createElement("div");
+            box.className = "box";
+            box.addEventListener("mouseover",function(){
+                box.style.backgroundColor = getcolour();
+            });
+            box.addEventListener("mouseleave",function(){
+                setTimeout(() => {
+                    box.style.backgroundColor="gray";
+                },1000);
+            })
+            container.appendChild(box);
+        }
+        function getcolour(){
+            const colour = 
+            "#"+
+            Math.floor(Math.random()*256*256*256)
+            .toString(16)
+            .padStart(6,"0");
+            return colour
+        }
